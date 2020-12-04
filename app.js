a1eefcadecf5b696e679f04619d6e7d19d310046
AnkitaBagale <manali.bagale@gmail.com>
2020-12-05 02:06:30 +0530

    initial commit: dev completed

---
+++ app.js
@@ -0,0 +1,48 @@
+
+
+var translateBtn= document.querySelector('#translateBtn');
+var userInput=document.querySelector('#userInput');
+var output = document.querySelector('#output');
+var serverUrl="https://api.funtranslations.com/translate/pirate.json";
+
+
+//adjust height of background image
+//fixing footer to bottom if screen height is bigger than content height;
+var contentHeight= document.querySelector('.place-over-bg').clientHeight;
+var minHeight = screen.height;
+var footer =document.querySelector('.footer');
+var img = document.querySelector('#bg');
+
+window.addEventListener('load', () => {
+    if(minHeight > contentHeight){
+        contentHeight = minHeight;
+        footer.classList.add('bottom0');
+    }
+    img.style.height = contentHeight+"px";
+  });
+
+//to add query parameter and key in api url
+function createUrl(text){
+    return encodeURI(serverUrl+"?"+"text="+text);
+}
+
+//to handle server down error
+function errorHandler(error){
+    console.log("Error is occurred"+error);
+    alert("Looks like server is down. Please try again after sometime")
+}
+
+//fetch the translated text from json
+function translate(){
+
+    fetch(createUrl(userInput.value))
+    .then(response => response.json())
+    .then(json => {
+        output.innerText= json.contents.translated
+    })
+    .catch(errorHandler);
+
+}
+
+//handling event occurance
+translateBtn.addEventListener("click", translate);
